aa33585ac9ec9cb06e09aeb2195139c433f8c86f
hdm0922 <hdm0922@gmail.com>
2025-09-20 17:01:43 +0900

    Add ClearTexture Function

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -14,6 +14,9 @@ export class Renderer
     public readonly SceneTexture    : GPUTexture;   // Texture To Render
     public readonly AccumTexture    : GPUTexture;   // Texture To Write Path-Traced Result
 
+    public readonly SceneTextureView: GPUTextureView;
+    public readonly AccumTextureView: GPUTextureView;
+
     // World Data
     public World                    : World;
 
@@ -48,13 +51,21 @@ export class Renderer
                 this.Canvas.height,
                 "rgba32float",
                 GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.COPY_SRC
-            )
+            );
+
+            this.SceneTextureView = this.SceneTexture.createView();
+            this.AccumTextureView = this.AccumTexture.createView();
         }
 
-        
+
     }
 
-
+    Initialize(): void
+    {
+        this.clearTexture(this.SceneTexture);
+        
+        return;
+    }
 
     Render(): void
     {
@@ -81,5 +92,24 @@ export class Renderer
         }
 
         return this.Device.createTexture(TextureDescriptor);
+    }
+
+
+
+    private clearTexture(Texture: GPUTexture): void
+    {
+
+        const bpp       = 16;
+        const unpadded  = Texture.width * bpp;
+        const padded    = Math.ceil(unpadded >> 8) << 8;
+
+        const DestinationInfo   : GPUTexelCopyTextureInfo       = { texture: Texture };
+        const DataToWrite       : GPUAllowSharedBufferSource    = new Uint8Array(Texture.height * padded);
+        const DataLayout        : GPUTexelCopyBufferLayout      = { bytesPerRow: padded, rowsPerImage: Texture.height };
+        const WriteSize         : GPUExtent3DStrict             = { width: Texture.width, height: Texture.height };
+
+        this.Device.queue.writeTexture(DestinationInfo, DataToWrite, DataLayout, WriteSize);
+
+        return;
     }
 };
